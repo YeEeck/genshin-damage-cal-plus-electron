3a8efb7e148a9aa473ff928c566e220bf3cfcb35
YeEeck <yechenkai@foxmail.com>
2021-10-27 23:08:08 +0800

    fix icon

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
     publicPath: "./", // 构建好的文件输出到哪里
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                "win": {
+                    "target": "nsis",
+                    "icon": "./icon1.ico"
+                }
+            }
+        },
+    },
 }
